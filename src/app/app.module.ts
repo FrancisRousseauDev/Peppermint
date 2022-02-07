f1b7b51466b686867ea98a8d027b8c369b1a371c
Francis Rousseau <frr@mybingli.com>
2022-02-07 13:28:16 +0100

    update css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { HotCollectionsComponent } from './components/hot-collections/hot-collections.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    HotCollectionsComponent
   ],
   imports: [
     BrowserModule,
